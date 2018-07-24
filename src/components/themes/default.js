6c498e79827af468bfb277084760508479dcd20d
Rafael Franco <rafaelfr@outlook.com>
2018-07-24 16:41:39 -0300

    Updated theme

--- src/components/themes/default.js
+++ src/components/themes/default.js
@@ -3,7 +3,8 @@ import { reversePalette } from 'styled-theme/composer'
 const theme = {}
 
 theme.palette = {
-  primary: ['#654EA3'],
+  primary: ['#503d81', '#5a4692', '#654ea3', '#8f7dc1', '#bcb1da'],
+  gray: ['#080808', '#212121', '#eeeeee', '#f7f7f7', '#ffffff'],
 }
 
 theme.reversePalette = reversePalette(theme.palette)
